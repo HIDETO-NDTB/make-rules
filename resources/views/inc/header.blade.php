adc0e8121d467021a286cd6ea2c6dab0d8ab13aa
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-29 17:40:38 +0530

    29.dec up21

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -2,8 +2,8 @@
     <div class="container">
       <div class="row centered">
         <div class="col-lg-8 col-lg-offset-2">
-          <h1>It Doesn't Take a Rocket <b>Scientist</b></h1>
-          <h2>It Takes a Designer</h2>
+          <h1></h1>
+          <h2></h2>
         </div>
       </div>
       <!-- row -->
