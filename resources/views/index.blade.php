6b3d6a8e5cc56c8d68d3a73fa02bee3a0b981afe
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-30 15:56:45 +0530

    30.dec up37

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -98,6 +98,65 @@
             </div>
             <a class="btn btn-primary" style="width: 300px;" href="">詳しく見る</a>
         </div>
+        <div class="comments">
+            <div class="col-lg-6">
+                <div class="samp border-primary">
+                    <div class="samp-body border-primary">
+                        <h4 class="samp-title">賛成派意見</h4>
+                        <p class="samp-text">条件付きですが賛成です。おかしな事件が増えているのを親のしつけの悪さが原因だとして、子供が子供を産んで育てている現状を変えた方が良いと常々思っていました。親になるからには必要最低限の経験と礼儀、そして現実を考えるとお金が必要です。条件付きですが賛成です。おかしな事件が増えているのを親のしつけの悪さが原因だとして、子供が子供を産んで育てている現状を変えた方が良いと常々思っていました。親になるからには必要最低限の経験と礼儀、そして現実を考えるとお金が必要です。</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card border-dark">
+                    <div class="card-body text-dark">
+                        <h4 class="card-title">反対派意見</h4>
+                        <p class="card-text">全面的に反対。少子高齢化が深刻な問題となっている日本でこのような規則を議論すること自体がおかしい。免許制にするばかりか、国がもっと適齢夫婦の出産・育児をサポートする法案が不可欠。それについては２週間以内にしっかりと考えて発議させて頂きます！！</p>
+                    </div>
+                </div>
+            </div>
+            <a class="btn btn-primary" style="width: 300px;" href="">詳しく見る</a>
+        </div>
+        <br>
+        <div class="comments">
+            <div class="col-lg-6">
+                <div class="samp border-primary">
+                    <div class="samp-body border-primary">
+                        <h4 class="samp-title">賛成派意見</h4>
+                        <p class="samp-text">日本国民であれば無条件で国が毎月お金をくれるというのに断る理由がありますか？貰える物は何でも貰うのが私の主義です。本気で要らない人は一度貰った上でユニセフにでも寄付すれば良いのではないでしょうか？</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card border-dark">
+                    <div class="card-body text-dark">
+                        <h4 class="card-title">反対派意見</h4>
+                        <p class="card-text">過剰に期待している方も多いですが、国民全員にお金を配るということは、国のマネーサプライが増えるということ。国にお金が増えるとインフレになり物価が上がります。毎月５万円もらっても全体の物価が上がることから生活レベルは一緒です。結局は日本の借金が増えるだけ。過剰に期待している方も多いですが、国民全員にお金を配るということは、国のマネーサプライが増えるということ。国にお金が増えるとインフレになり物価が上がります。毎月５万円もらっても全体の物価が上がることから生活レベルは一緒です。結局は日本の借金が増えるだけ</p>
+                    </div>
+                </div>
+            </div>
+            <a class="btn btn-primary" style="width: 300px;" href="">詳しく見る</a>
+        </div>
+        <br>
+        <div class="comments">
+            <div class="col-lg-6">
+                <div class="samp border-primary">
+                    <div class="samp-body border-primary">
+                        <h4 class="samp-title">賛成派意見</h4>
+                        <p class="samp-text">条件付きですが賛成です。おかしな事件が増えているのを親のしつけの悪さが原因だとして、子供が子供を産んで育てている現状を変えた方が良いと常々思っていました。親になるからには必要最低限の経験と礼儀、そして現実を考えるとお金が必要です。</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card border-dark">
+                    <div class="card-body text-dark">
+                        <h4 class="card-title">反対派意見</h4>
+                        <p class="card-text">全面的に反対。少子高齢化が深刻な問題となっている日本でこのような規則を議論すること自体がおかしい。免許制にするばかりか、国がもっと適齢夫婦の出産・育児をサポートする法案が不可欠。それについては２週間以内にしっかりと考えて発議させて頂きます！！</p>
+                    </div>
+                </div>
+            </div>
+            <a class="btn btn-primary" style="width: 300px;" href="">詳しく見る</a>
+        </div>
 
 
         <p><br/><br/></p>
