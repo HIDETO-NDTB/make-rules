2207edd254ab505da8958e698ce94b16d4ae6885
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-30 19:09:02 +0530

    30.dec up104

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,11 +6,11 @@
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a class="btn btn-default" href="#">国　自治体</a></li>
-                <li><a class="btn btn-default" href="#">会社</a></li>
-                <li><a class="btn btn-default" href="#">学校</a></li>
-                <li><a class="btn btn-default" href="#">家族　親戚</a></li>
-                <li><a class="btn btn-default" href="#">男女　恋愛</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin-right: 10px;" href="#">国　自治体</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin-right: 10px;" href="#">会社</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin-right: 10px;" href="#">学校</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin-right: 10px;" href="#">家族　親戚</a></li>
+                <li><a class="btn btn-default" style="width:120px" href="#">男女　恋愛</a></li>
             </ul>
         </div>
         <!--/.nav-collapse -->
