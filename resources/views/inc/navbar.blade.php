b0ae14784f3ab3a911bea1f1b4ccb1c56ddbcb2f
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-30 19:13:23 +0530

    30.dec up106

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,11 +6,11 @@
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a class="btn btn-default" style="width:120px; margin-right: auto 10px auto 0;" href="#">国　自治体</a></li>
-                <li><a class="btn btn-default" style="width:120px; margin-right: auto 10px auto 0;" href="#">会社</a></li>
-                <li><a class="btn btn-default" style="width:120px; margin-right: auto 10px auto 0;" href="#">学校</a></li>
-                <li><a class="btn btn-default" style="width:120px; margin-right: auto 10px auto 0;" href="#">家族　親戚</a></li>
-                <li><a class="btn btn-default" style="width:120px" href="#">男女　恋愛</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin: auto 5px;" href="#">国　自治体</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin: auto 5px;" href="#">会社</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin: auto 5px;" href="#">学校</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin: auto 5px;" href="#">家族　親戚</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin: auto 5px;" href="#">男女　恋愛</a></li>
             </ul>
         </div>
         <!--/.nav-collapse -->
