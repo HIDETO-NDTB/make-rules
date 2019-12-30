2ca701736f7b4cfd41d9e95ff7a1ed5e0977b9e7
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-30 20:05:12 +0530

    30.dec up122

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,24 +1,28 @@
 <!-- Fixed navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <h1>Make Rules</h1>
-        </div>
+        <div class="col-lg-3">
+            <div class="navbar-header">
 
-    </div>
-    <div class="navbar-collapse collapse">
-        <ul class="nav navbar-nav navbar-right">
-            <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">国　自治体</a></li>
-            <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">会社</a></li>
-            <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">学校</a></li>
-            <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">家族　親戚</a></li>
-            <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">男女　恋愛</a></li>
-        </ul>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                    <h1>Make Rules</h1>
+            </div>
+        </div>
+        <div class="col-lg-9">
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">国　自治体</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">会社</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">学校</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">家族　親戚</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">男女　恋愛</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
         <!--/.nav-collapse -->
 </div>
