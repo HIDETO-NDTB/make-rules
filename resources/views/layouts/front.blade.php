748be3ef5c7f57ed790a631f872da7c75e2e572c
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-30 18:35:29 +0530

    30.dec up88

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -50,16 +50,14 @@
       <!-- col-lg-4 -->
 
       <div class="col-lg-4">
-        <i class="far fa-folder-open"></i>
-        <h4><span>新</span> 発議</h4>
+        <h4><i class="far fa-folder-open"></i><span>新</span>  発議</h4>
         <br>
         <li><a href="#">学校制服廃止法案<br>（２月３日発議）</a></li><br>
       </div>
       <!-- col-lg-4 -->
 
       <div class="col-lg-4">
-        <i class="fas fa-search-location"></i>
-        <h4><span>熱</span> 注目</h4>
+        <h4><i class="fas fa-search-location"></i><span>熱</span>  注目</h4>
         <br>
         <li><a href="#">出産を免許制にしませんか？<br>（コメント数９７件）</a></li><br>
         <li><a href="#">ベーシックインカム。全国民に毎月５万円を支給<br>（コメント数７３件）</a></li>
