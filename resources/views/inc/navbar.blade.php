ab744c77a2381b446b1e5a92d48a07f68730b6a4
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-30 19:16:53 +0530

    30.dec up108

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,7 +6,7 @@
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">国　自治体</a></li>
+                <li><a class="btn btn-default" style="width:120px; margin: 10px 5px; color:black;" href="#">国　自治体</a></li>
                 <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">会社</a></li>
                 <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">学校</a></li>
                 <li><a class="btn btn-default" style="width:120px; margin: 10px 5px;" href="#">家族　親戚</a></li>
