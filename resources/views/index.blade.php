63d89ad1ad20894f43599f3af5dbc723e2c17c0d
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-31 16:15:20 +0530

    31.dec up39

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,51 @@
 @extends('layouts.front')
 @section('page')
 
+<div id="headerwrap">
+    <div class="container">
+        <div class="row centered">
+            <div class="col-lg-8 col-lg-offset-2">
+              <h1></h1>
+              <h2></h2>
+            </div>
+        </div>
+      <!-- row -->
+    </div>
+    <!-- container -->
+</div>
+<!-- headerwrap -->
+
+<div class="container w">
+    <div class="row centered">
+        <br><br>
+        <div class="col-lg-4">
+            <h4><i class="fas fa-hammer"></i><span>近</span>  投票</h4>
+            <br>
+            <li><a href="#">ベーシックインカム。全国民に毎月５万円を支給<br>（あと３日で投票日）</a></li><br>
+            <li><a href="#">毎年１回は海外旅行！<br>（あと５日で投票日）</a></li>
+        </div>
+        <!-- col-lg-4 -->
+
+        <div class="col-lg-4">
+            <h4><i class="far fa-folder-open"></i><span>新</span>  発議</h4>
+            <br>
+            <li><a href="#">学校制服廃止法案<br>（２月３日発議）</a></li><br>
+        </div>
+      <!-- col-lg-4 -->
+
+        <div class="col-lg-4">
+            <h4><i class="fas fa-search-location"></i><span>熱</span>  注目</h4>
+            <br>
+            <li><a href="#">出産を免許制にしませんか？<br>（コメント数９７件）</a></li><br>
+            <li><a href="#">ベーシックインカム。全国民に毎月５万円を支給<br>（コメント数７３件）</a></li>
+        </div>
+      <!-- col-lg-4 -->
+    </div>
+    <!-- row -->
+    <br>
+    <br>
+</div>
+
 <div id="dg">
     <div class="container">
       <div class="row centered">
