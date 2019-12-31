f1a69632585280b5c29c11f10251a4b8cab111a3
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-31 15:39:25 +0530

    31.dec up38

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,13 +1,13 @@
 <div id="headerwrap">
     <div class="container">
-      <div class="row centered">
-        <div class="col-lg-8 col-lg-offset-2">
-          <h1></h1>
-          <h2></h2>
+        <div class="row centered">
+            <div class="col-lg-8 col-lg-offset-2">
+              <h1></h1>
+              <h2></h2>
+            </div>
         </div>
-      </div>
       <!-- row -->
     </div>
     <!-- container -->
-  </div>
-  <!-- headerwrap -->
+</div>
+<!-- headerwrap -->
