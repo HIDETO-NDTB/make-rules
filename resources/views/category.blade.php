b607e5066966c6490d9d085a44417fddc506c280
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2019-12-31 19:58:28 +0530

    31.dec up39

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -8,7 +8,7 @@
                     <!-- START PRICING TABLE -->
                     <div class="pricing-option">
                         <div class="pricing-top">
-                            <span class="pricing-edition">国 自治体　未投票案件数</span>
+                            <span class="pricing-edition">国　自治体<br>未投票案件数</span>
                             <span class="price">
                                 <sup></sup>
                                 <span class="price-amount">４</span>
