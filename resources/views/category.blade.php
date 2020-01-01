8c702f31544d0124d3e542bde6323c028f767e4d
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-01 19:19:57 +0530

    1.jan up

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -92,7 +92,7 @@
                         <div class="row">
                             <div class="col-lg-8"></div>
                             <div class="col-lg-4">
-                            <a class="btn btn-primary" style="width: 300px;" href="">コメントする</a>
+                            <a class="btn btn-danger" style="width: 300px;" href="">投票に行く</a>
                         </div>
                     </ul>
                 </div>
