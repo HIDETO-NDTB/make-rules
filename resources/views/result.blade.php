27b263b25e1720103243f5bbf210d0d744397ec7
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-01 20:52:52 +0530

    1.jan up

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -6,7 +6,7 @@
         <div class="row centered">
             <div class="col-lg-6">
                 <div class="card2">
-                    <div class="card-header"><h4><b>ベーシックインカム。全国民に毎月５万円を支給</b></h4></div>
+                    <div class="card-header"><h4><b>エアコンの設定温度は女性が決める</b></h4></div>
                     <div class="card-body">
                         <ul>
                             <div class="row">
@@ -19,11 +19,11 @@
                                 <div class="col-lg-8"></div>
                             </div>
                             <div class="row">
-                                <li><p>全日本国民に毎月５万円ずつ無条件で国が支給。目的は日本人がより人間らしく生きること。今までより仕事の時間を減らし趣味や芸術に打ち込むことで日本のサブカルチャー育成にも一役買うことに繋がると思われる。支給対象は日本人のみで、永住権があろうと外国人は対象とはせず。</p></li>
+                                <li><p>長く議論されてきた男女の温度差に決着をつけませんか？ 私は男ですが、やはり温度管理は女性に任せた方が世の中うまくいくと思います。</p></li>
                             </div>
                             <div class="row">
-                                <li>発議日: 2020年1月30日</li>
-                                <li>投票日: 2020年2月6日</li>
+                                <li>発議日: 2020年1月25日</li>
+                                <li>投票日: 2020年2月1日</li>
                             </div>
                         </ul>
                     </div>
