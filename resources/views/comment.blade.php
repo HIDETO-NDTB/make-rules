32c275487799792ea25618f459e76e7ee9765cdc
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-01 15:03:31 +0530

    1.jan up

--- resources/views/comment.blade.php
+++ resources/views/comment.blade.php
@@ -1,6 +1,34 @@
 @extends('layouts/front')
 @section('page')
 
+<div id="commentwrap">
+    <div class="container">
+        <div class="row centered">
+                <div class="col-lg-6">
+                    <!-- START PRICING TABLE -->
+                    <div class="pricing-option">
+                        <div class="pricing-top">
+                            <span class="pricing-edition">国　自治体<br>未投票案件数</span>
+                            <span class="price">
+                                <sup></sup>
+                                <span class="price-amount">４</span>
+                                <small>案件</small>
+                            </span>
+                        </div>
+                        <ul>
+                            <li>総発議数　<strong>９７案件</strong></li>
+                            <li>投票済数　<strong>９３案件</strong></li>
+                            <li>採用数　<strong>２０案件</strong></li>
+                        </ul>
+                    </div>
+                    <!-- /pricing-option -->
+                    <!-- END PRICING TABLE -->
+                </div>
 
+        </div>
+      <!-- row -->
+    </div>
+    <!-- container -->
+</div>
 
 @endsection
