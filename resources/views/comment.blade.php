8a73b121ace0b1ff837784f11b8168845c4e0ab4
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-01 15:55:32 +0530

    1.jan up

--- resources/views/comment.blade.php
+++ resources/views/comment.blade.php
@@ -5,24 +5,34 @@
     <div class="container">
         <div class="row centered">
             <div class="col-lg-4">
-                <!-- START PRICING TABLE -->
-                <div class="pricing-option">
-                    <div class="pricing-top">
-                        <span class="pricing-edition">国　自治体<br>未投票案件数</span>
-                        <span class="price">
-                            <sup></sup>
-                            <span class="price-amount">４</span>
-                            <small>案件</small>
-                        </span>
+                <div class="card">
+                    <div class="card-header"><h4>ベーシックインカム。全国民に毎月５万円を支給</h4></div>
+                    <div class="card-body">
+                        <ul>
+                            <div class="row">
+                                <div class="col-lg-1">
+                                    <li><img src="{{ asset('app/img/pic3.jpg') }}" class="img-responsive img-circle" style="width:60px; height:60px;" alt="avator"></li>
+                                </div>
+                                <div class="col-lg-1">
+                                    <h5>首藤英人</h5>
+                                </div>
+                                <div class="col-lg-10"></div>
+                            </div>
+                            <div class="row">
+                                <li><p>全日本国民に毎月５万円ずつ無条件で国が支給。目的は日本人がより人間らしく生きること。今までより仕事の時間を減らし趣味や芸術に打ち込むことで日本のサブカルチャー育成にも一役買うことに繋がると思われる。支給対象は日本人のみで、永住権があろうと外国人は対象とはせず。</p></li>
+                            </div>
+                            <div class="row">
+                                <li>発議日: 2020年1月30日</li>
+                                <li>投票日: 2020年2月6日</li>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-8"></div>
+                                <div class="col-lg-4">
+                                <a class="btn btn-primary" style="width: 300px;" href="{{ route('comment') }}">コメントする</a>
+                            </div>
+                        </ul>
                     </div>
-                    <ul>
-                        <li>総発議数　<strong>９７案件</strong></li>
-                        <li>投票済数　<strong>９３案件</strong></li>
-                        <li>採用数　<strong>２０案件</strong></li>
-                    </ul>
                 </div>
-                <!-- /pricing-option -->
-                <!-- END PRICING TABLE -->
             </div>
             <div class="col-lg-8">
                 <img src="{{ asset('app/img/comment-min.jpg') }}"  style="width:100%; height:100%;">
