83a95313fc81a2d8694388287d83d8c61bc8e3aa
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-01 18:26:12 +0530

    1.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -9,23 +9,23 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                    <a class="title" href="#">Make Rules</a>
+                    <a class="title" href="{{ route('index') }}">Make Rules</a>
             </div>
         </div>
         <div class="col-lg-6">
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-left">
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px; color:black;" href="{{ route('category') }}">国　自治体</a></li>
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px; color:black;" href="#">会社</a></li>
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px; color:black;" href="#">学校</a></li>
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px; color:black;" href="#">家族　親戚</a></li>
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 5px; color:black;" href="#">男女　恋愛</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px; color:black;" href="{{ route('category') }}">国　自治体</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px; color:black;" href="#">会社</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px; color:black;" href="#">学校</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px; color:black;" href="#">家族　親戚</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px; color:black;" href="#">男女　恋愛</a></li>
                 </ul>
             </div>
         </div>
         <div class="col-lg-3">
             <li class="nav-item navbar-right">
-                <a class="btn btn-primary" style="width:120px; height:50px; margin: 15px 30px;" href="">Login</a>
+                <a class="btn btn-primary" style="width:120px; height:40px; margin: 10px 30px;" href="">Login</a>
             </li>
         </div>
     </div>
