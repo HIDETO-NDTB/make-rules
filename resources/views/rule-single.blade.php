2b739981512898bcbdc9bdac49e8dfd2bda71774
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-01 18:12:35 +0530

    1.jan up

--- resources/views/rule-single.blade.php
+++ resources/views/rule-single.blade.php
@@ -110,6 +110,76 @@
                     </ul>
                 </div>
             </div>
+            <br>
+            <div class="card">
+                <div class="card-header"><h4>反対派意見</h4></div>
+                <div class="card-body">
+                    <ul>
+                        <div class="row">
+                            <div class="col-lg-1">
+                                <li><img src="{{ asset('app/img/pic3.jpg') }}" class="img-responsive img-circle" style="width:60px; height:60px;" alt="avator"></li>
+                            </div>
+                            <div class="col-lg-1">
+                                <h5>首藤英人</h5>
+                            </div>
+                            <div class="col-lg-10"></div>
+                        </div>
+                        <div class="row">
+                            <li><p>過剰に期待している方も多いですが、国民全員にお金を配るということは、国のマネーサプライが増えるということ。国にお金が増えるとインフレになり物価が上がります。毎月５万円もらっても全体の物価が上がることから生活レベルは一緒です。結局は日本の借金が増えるだけ</p></li>
+                        </div>
+                        <div class="row">
+                            <li>発言日: 2020年2月3日</li>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-header"><h4>反対派意見</h4></div>
+                <div class="card-body">
+                    <ul>
+                        <div class="row">
+                            <div class="col-lg-1">
+                                <li><img src="{{ asset('app/img/pic3.jpg') }}" class="img-responsive img-circle" style="width:60px; height:60px;" alt="avator"></li>
+                            </div>
+                            <div class="col-lg-1">
+                                <h5>首藤英人</h5>
+                            </div>
+                            <div class="col-lg-10"></div>
+                        </div>
+                        <div class="row">
+                            <li><p>過剰に期待している方も多いですが、国民全員にお金を配るということは、国のマネーサプライが増えるということ。国にお金が増えるとインフレになり物価が上がります。毎月５万円もらっても全体の物価が上がることから生活レベルは一緒です。結局は日本の借金が増えるだけ</p></li>
+                        </div>
+                        <div class="row">
+                            <li>発言日: 2020年2月3日</li>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-header"><h4>賛成派意見</h4></div>
+                <div class="card-body">
+                    <ul>
+                        <div class="row">
+                            <div class="col-lg-1">
+                                <li><img src="{{ asset('app/img/pic3.jpg') }}" class="img-responsive img-circle" style="width:60px; height:60px;" alt="avator"></li>
+                            </div>
+                            <div class="col-lg-1">
+                                <h5>首藤英人</h5>
+                            </div>
+                            <div class="col-lg-10"></div>
+                        </div>
+                        <div class="row">
+                            <li><p>日本国民であれば無条件で国が毎月お金をくれるというのに断る理由がありますか？貰える物は何でも貰うのが私の主義です。本気で要らない人は一度貰った上でユニセフにでも寄付すれば良いのではないでしょうか？</p></li>
+                        </div>
+                        <div class="row">
+                            <li>発言日: 2020年2月3日</li>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+            <br>
         </div>
     </div>
 </div>
