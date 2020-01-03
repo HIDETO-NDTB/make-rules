c4b1a2c50267ee0af0f58993fcb03ad6940e22c0
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-03 15:12:05 +0530

    3.jan up

--- resources/views/initiative.blade.php
+++ resources/views/initiative.blade.php
@@ -4,33 +4,8 @@
 <div id="commentwrap">
     <div class="container">
         <div class="row centered">
-            <div class="col-lg-6">
-                <div class="card2">
-                    <div class="card-header"><h4><b>ベーシックインカム。全国民に毎月５万円を支給</b></h4></div>
-                    <div class="card-body">
-                        <ul>
-                            <div class="row">
-                                <div class="col-lg-2">
-                                    <li><img src="{{ asset('app/img/pic3.jpg') }}" class="img-responsive img-circle" style="width:60px; height:60px;" alt="avator"></li>
-                                </div>
-                                <div class="col-lg-2">
-                                    <h5>首藤英人</h5>
-                                </div>
-                                <div class="col-lg-8"></div>
-                            </div>
-                            <div class="row">
-                                <li><p>全日本国民に毎月５万円ずつ無条件で国が支給。目的は日本人がより人間らしく生きること。今までより仕事の時間を減らし趣味や芸術に打ち込むことで日本のサブカルチャー育成にも一役買うことに繋がると思われる。支給対象は日本人のみで、永住権があろうと外国人は対象とはせず。</p></li>
-                            </div>
-                            <div class="row">
-                                <li>発議日: 2020年1月30日</li>
-                                <li>投票日: 2020年2月6日</li>
-                            </div>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <img src="{{ asset('app/img/comment-min.jpg') }}"  style="width:100%; height:100%;">
+            <div class="col-lg-12">
+                <img src="{{ asset('app/img/initiative-min.jpg') }}"  style="width:100%; height:100%;">
             </div>
         </div>
       <!-- row -->
