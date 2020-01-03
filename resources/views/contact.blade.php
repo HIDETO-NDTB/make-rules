6d07a613a65c54afbde1490388d9a2f847338cc0
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-03 19:47:48 +0530

    3.jan up

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -17,28 +17,33 @@
         <div class="row centerd">
             <div class="col-lg-12">
                 <h4><span> お問い合わせフォーム </span></h4>
+                <p>Make Rulesをご利用頂き、誠にありがとうございます。<br>皆様より頂戴した貴重なご意見は今後の運営に活かして参ります。</p>
                 <form action="" method="POST">
                     @csrf
                     <div class="form-group">
-                        <label>タイトル（●文字以内）</label>
-                        <input type="text" name="title" class="form-control" placeholder="タイトルを入力下さい">
-                    </div>
-                    <div class="form-group">
                         <label>カテゴリ</label><br>
                         <select name="category" style="width: 200px;">
-                            <option value="country">国 自治体</option>
-                            <option value="company">会社</option>
-                            <option value="school">学校</option>
-                            <option value="family">家族 親戚</option>
-                            <option value="love">男女 恋愛</option>
+                            <option value="country">Make Rules に関するお問い合わせ</option>
+                            <option value="company">不具合報告</option>
+                            <option value="school">広告に関するお問い合わせ</option>
+                            <option value="family">ビジネスに関するお問い合わせ</option>
+                            <option value="love">その他のお問い合わせ</option>
                         </select>
                     </div>
                     <div class="form-group">
-                        <label>内容（●文字以内）</label>
+                        <label>氏名<span>(*必須）</span></label>
+                        <input type="text" name="name" stile="width: 300px" placeholder="氏名">
+                    </div>
+                    <div class="form-group">
+                        <label>Email<span>(*必須）</span></label>
+                        <input type="email" name="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>お問い合わせ内容（●文字以内）</label>
                         <textarea name="content" id="" cols="30" rows="10" class="form-control" placeholder="内容を入力下さい"></textarea>
                     </div>
                     <div class="form-group">
-                        <button type="submit" class="btn btn-danger" style="width: 300px;">発議する</button>
+                        <button type="submit" class="btn btn-danger" style="width: 300px;">送信する</button>
                     </div>
                 </form>
             </div>
