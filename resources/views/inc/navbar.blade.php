5c239fdae524408c00f02cc47596f380efa349ae
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-03 17:21:00 +0530

    3.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- Fixed navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-8">
             <div class="navbar-header">
 
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -12,12 +12,12 @@
                 <a class="title" href="{{ route('index') }}">Make Rules</a>
             </div>
         </div>
-        <div class="col-lg-3 col-md-6 col-sm-3 col-xs-12">
+        <div class="col-lg-3 col-md-6 col-sm-3 col-xs-3">
             <li class="nav-item navbar-right">
                 <a class="btn btn-primary" style="width:100px;" href="">Login</a>
             </li>
         </div>
-        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-9">
+        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-1">
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
                     <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="{{ route('category') }}">国　自治体</a></li>
