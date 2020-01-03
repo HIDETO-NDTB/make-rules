8e89226fd2a1dcc9795ced1b2011e5804aa8c77b
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-03 15:17:35 +0530

    3.jan up

--- resources/views/initiative.blade.php
+++ resources/views/initiative.blade.php
@@ -4,7 +4,7 @@
 <div id="commentwrap">
     <div class="container-fluid">
         <div class="col-lg-12">
-            <img src="{{ asset('app/img/initiative-min.jpg') }}"  style="width:120%; height:80%;">
+            <img src="{{ asset('app/img/initiative-min.jpg') }}"  style="width:100%; height:60%;">
         </div>
     </div>
     <!-- container -->
