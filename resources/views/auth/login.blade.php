45d1138bac89c7f2208e233969bdd9e018643715
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-05 17:30:02 +0530

    5.jan up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,7 +65,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary" style="width: 200px">
-                                    {{ __('Login') }}
+                                    {{ __('ログイン') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
