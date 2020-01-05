5e3eac838bcfc126128a1dbb0a51271bb1bf469d
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-05 20:40:01 +0530

    5.jan up

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,7 +39,7 @@
                         <div class="form-group row">
                             <label for="age" class="col-md-4 col-form-label text-md-right">{{ __('年齢') }}</label>
                             <div class="col-md-6">
-                                <input id="age" type="integer" name="age" value="int">
+                                <input id="age" type="int" name="age" value="int">
                             </div>
                         </div>
 
