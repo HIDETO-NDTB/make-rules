5001af239d39e7e15303c0df43537060832771c6
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-05 18:10:30 +0530

    5.jan up

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,7 +39,7 @@
                         <div class="form-group row">
                             <label for="age" class="col-md-4 col-form-label text-md-right">{{ __('年齢') }}</label>
                             <div class="col-md-6">
-                                <input id="age" type="text" class="form-control" name="age" value="" autofocus>
+                                <input id="age" type="text" name="age" value="" autofocus>
                             </div>
                         </div>
 
