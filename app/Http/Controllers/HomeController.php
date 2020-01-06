85e043418d113c62faf41650fa60da7ea711855b
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-06 19:36:43 +0530

    6.jan up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home')->with('categories',Category::all());
+        return view('home');
     }
 }
