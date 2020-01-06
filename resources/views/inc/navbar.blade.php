6de40a2835b53f629bed24a477d63924a239620e
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-06 12:23:06 +0530

    6.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -12,14 +12,25 @@
                 <a class="title" href="{{ route('index') }}">Make Rules</a>
             </div>
         </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-6">
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="{{ route('category') }}">国　自治体</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="#">会社</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="#">学校</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="#">家族　親戚</a></li>
+                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="#">男女　恋愛</a></li>
+                </ul>
+            </div>
+        </div>
         <div class="col-lg-3 col-md-6 col-sm-3 col-xs-3">
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
                 @guest
-
+                    <li class="nav-item">
                         <a class="btn btn-primary" style="width: 100px; color:white;" href="{{ route('login') }}">{{ __('Login') }}</a>
-
+                    </li>
                 @else
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -40,17 +51,6 @@
                     </li>
                 @endguest
             </ul>
-        </div>
-        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-6">
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="{{ route('category') }}">国　自治体</a></li>
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="#">会社</a></li>
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="#">学校</a></li>
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="#">家族　親戚</a></li>
-                    <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="#">男女　恋愛</a></li>
-                </ul>
-            </div>
         </div>
     </div>
         <!--/.nav-collapse -->
