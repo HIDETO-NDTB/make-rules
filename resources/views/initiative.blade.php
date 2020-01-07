5d0b0049128d36270cfd769a2c407b8289fab6af
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-07 19:13:08 +0530

    7.jan up

--- resources/views/initiative.blade.php
+++ resources/views/initiative.blade.php
@@ -17,34 +17,34 @@
         <div class="row centerd">
             <div class="col-lg-12">
                 <h4><span> 新法案の発議 </span></h4>
-                    <form action="{{ route('rules.store') }}" method="POST">
-                        @csrf
+                <form action="{{ route('rules.store') }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label>タイトル（20文字以内）</label>
+                        <input type="text" name="title" class="form-control" placeholder="タイトルを入力下さい">
+                    </div>
+                    <div class="form-group">
+                        <label>カテゴリ</label><br>
+                        <select name="category_id" style="width: 200px;">
+                            <option value="1">国 自治体</option>
+                            <option value="2">会社</option>
+                            <option value="3">学校</option>
+                            <option value="4">家族 親戚</option>
+                            <option value="5">男女 恋愛</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label>内容（●文字以内）</label>
+                        <textarea name="content" id="" cols="30" rows="10" class="form-control" placeholder="内容を入力下さい"></textarea>
+                    </div>
+                    @if(Auth::check())
                         <div class="form-group">
-                            <label>タイトル（20文字以内）</label>
-                            <input type="text" name="title" class="form-control" placeholder="タイトルを入力下さい">
+                            <button type="submit" class="btn btn-danger" style="width: 300px;">発議する</button>
                         </div>
-                        <div class="form-group">
-                            <label>カテゴリ</label><br>
-                            <select name="category_id" style="width: 200px;">
-                                <option value="1">国 自治体</option>
-                                <option value="2">会社</option>
-                                <option value="3">学校</option>
-                                <option value="4">家族 親戚</option>
-                                <option value="5">男女 恋愛</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label>内容（●文字以内）</label>
-                            <textarea name="content" id="" cols="30" rows="10" class="form-control" placeholder="内容を入力下さい"></textarea>
-                        </div>
-                        @if(Auth::check())
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-danger" style="width: 300px;">発議する</button>
-                            </div>
-                    </form>
-                @else
-                    <h5>申し訳ありません。ログインの上で発議下さい。</h5>
-                @endif
+                    @else
+                        <h5>申し訳ありません。ログインの上で発議下さい。</h5>
+                    @endif
+                </form>
             </div>
         </div>
     </div>
