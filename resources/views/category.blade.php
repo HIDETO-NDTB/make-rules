0b83dac30a1cc693382dd2fca65a65f8bd634072
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-07 20:15:07 +0530

    7.jan up

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -32,10 +32,12 @@
 </div>
 
 <div class="container w">
+    @if($categories->count() > 0)
     <div class="row centered">
         <div class="col-lg-12">
+            @foreach($categories as $category)
             <div class="card">
-                <div class="card-header"><h4>ベーシックインカム。全国民に毎月５万円を支給</h4></div>
+                <div class="card-header"><h4>{{ $category->name }}</h4></div>
                 <div class="card-body">
                     <ul>
                         <div class="row">
@@ -48,12 +50,13 @@
                             <div class="col-lg-10"></div>
                         </div>
                         <div class="row">
-                            <li><p>全日本国民に毎月５万円ずつ無条件で国が支給。目的は日本人がより人間らしく生きること。今までより仕事の時間を減らし趣味や芸術に打ち込むことで日本のサブカルチャー育成にも一役買うことに繋がると思われる。支給対象は日本人のみで、永住権があろうと外国人は対象とはせず。</p></li>
+                            <li><p>{{ $category->content }}</p></li>
                         </div>
                         <div class="row">
-                            <li>発議日: 2020年1月30日</li>
-                            <li>投票日: 2020年2月6日</li>
+                            <li>発議日: {{ $category->initiative_date }}</li>
+                            <li>投票日: {{ $category->vote_date }}</li>
                         </div>
+                        @endforeach
                         <div class="row">
                             <div class="col-lg-8"></div>
                             <div class="col-lg-4">
@@ -62,6 +65,7 @@
                     </ul>
                 </div>
             </div>
+            @endif
         </div>
     </div>
 </div>
