5823c4669567eee50dcff9d816260e552f867e11
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-07 17:57:32 +0530

    7.jan up

--- app/Rule.php
+++ app/Rule.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rule extends Model
 {
+
+    protected $fillable = ['title','content','status','initiative_date','vote_date','category_id','user_id'];
+
     public function category(){
         return $this->belongsTo('App\Category');
     }
