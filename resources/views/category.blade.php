48f23adfb1329c3a6c00a8fc1f3b1796ce7d3541
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-08 15:53:41 +0530

    8.jan up

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -45,7 +45,7 @@
                                 <li><img src="{{ asset('app/img/pic3.jpg') }}" class="img-responsive img-circle" style="width:60px; height:60px;" alt="avator"></li>
                             </div>
                             <div class="col-lg-1">
-                                <h5>{{ $rule->user->name }}</h5>
+                                <h5>{{ $rule->users->name }}</h5>
                             </div>
                             <div class="col-lg-10"></div>
                         </div>
