3b29aa52ca07b3ce93f4f9ac0fc3fd124e3ff683
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-08 18:05:22 +0530

    8.jan up

--- resources/views/rule-single.blade.php
+++ resources/views/rule-single.blade.php
@@ -5,7 +5,6 @@
     <div class="container">
         <div class="row centered">
             <div class="col-lg-6">
-
                 <div class="card2">
                     <div class="card-header"><h4><b>{{ $rule->title }}</b></h4></div>
                     <div class="card-body">
@@ -15,21 +14,20 @@
                                     <li><img src="{{ asset('app/img/pic3.jpg') }}" class="img-responsive img-circle" style="width:60px; height:60px;" alt="avator"></li>
                                 </div>
                                 <div class="col-lg-2">
-                                    <h5>首藤英人</h5>
+                                    <h5>{{ $rule->user->name }}</h5>
                                 </div>
                                 <div class="col-lg-8"></div>
                             </div>
                             <div class="row">
-                                <li><p>全日本国民に毎月５万円ずつ無条件で国が支給。目的は日本人がより人間らしく生きること。今までより仕事の時間を減らし趣味や芸術に打ち込むことで日本のサブカルチャー育成にも一役買うことに繋がると思われる。支給対象は日本人のみで、永住権があろうと外国人は対象とはせず。</p></li>
+                                <li><p>{{ $rule->content }}</p></li>
                             </div>
                             <div class="row">
-                                <li>発議日: 2020年1月30日</li>
-                                <li>投票日: 2020年2月6日</li>
+                                <li>発議日: {{ $rule->initiative_date }}</li>
+                                <li>投票日: {{ $rule->vote_date }}</li>
                             </div>
                         </ul>
                     </div>
                 </div>
-
             </div>
             <div class="col-lg-6">
                 <img src="{{ asset('app/img/comment-min.jpg') }}"  style="width:100%; height:100%;">
