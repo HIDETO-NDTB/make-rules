e43610f3fe8005d8f10fc2d39931fe8cdfe2ec43
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-09 14:32:03 +0530

    9.jan up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -61,16 +61,21 @@
         <p>最近可決された法案です。（妄想の中で）遵守しましょう。</p>
         <div class="col-lg-4">
           <div class="tilt">
-
+            @isset($first_result)
             <a href="{{ route('result') }}"><img src="{{ asset('app/img/image01.jpeg') }}" alt=""></a>
             <label>{{ $first_result->title }}</label>
+            @else
+            <p>最近可決された法案はありません。</p>
+            @endisset
           </div>
         </div>
 
         <div class="col-lg-4">
           <div class="tilt">
+            @isset($second_result)
             <a href="#"><img src="{{ asset('app/img/image01.jpeg') }}" alt=""></a>
             <label>{{ $second_result->title }}</label>
+            @endisset
           </div>
         </div>
 
