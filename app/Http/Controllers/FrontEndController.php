7db2e918ba2262c275b46eb1852b643d4d8d0c4f
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-09 13:13:14 +0530

    9.jan up

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -14,11 +14,13 @@ class FrontEndController extends Controller
         $first_initiative = Rule::orderBy('initiative_date','desc')->first();
         $second_initiative = Rule::orderBy('initiative_date','desc')->take(1)->skip(1)->get()->first();
         $third_initiative = Rule::orderBy('initiative_date','desc')->take(1)->skip(2)->get()->first();
+        $first_result = Rule::orderBy('vote_date','desc')->where('status','=',1)->first();
         $categories = Category::all();
 
         return view('index')->with('first_initiative',$first_initiative)
                             ->with('second_initiative',$second_initiative)
                             ->with('third_initiative',$third_initiative)
+                            ->with('$first_result',$$first_result)
                             ->with('categories',$categories);
     }
 
