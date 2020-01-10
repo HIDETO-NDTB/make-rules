b5db5e5e1d7f5570011220f97deb3d981422b3fe
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-10 19:46:40 +0530

    10.jan up

--- app/Rule.php
+++ app/Rule.php
@@ -13,9 +13,9 @@ class Rule extends Model
         return $this->belongsTo('App\Category');
     }
 
-    public function user(){
+    /*public function user(){
         return $this->belongsTo('App\User');
-    }
+    }*/
 
     public function users(){
         return $this->belongsToMany('App\User')
