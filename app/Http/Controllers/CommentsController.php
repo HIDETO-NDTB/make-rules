20114a654907ca9818396b60e63ed0b70458c693
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-10 15:58:34 +0530

    10.jan up

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -55,7 +55,7 @@ class CommentsController extends Controller
 
         $comment->save();
 
-        $comment->rules()->attach($rule_id);
+        $user->rules()->attach($rule_id);
 
         Session::flash('success','コメントされました');
         return redirect()->route('initiative');
