27ad7372ecb29975da1c324e5642d8bc8674093c
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-10 22:11:23 +0530

    10.jan up

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'age' , 'gender' , 'email', 'password',
+        'name', 'age' , 'gender' , 'email', 'password','opinion_id'
     ];
 
     /**
