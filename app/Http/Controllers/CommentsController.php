fe931efdf4863f01455001b4383936aa7a45688d
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-10 17:33:17 +0530

    10.jan up

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Comment;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
 class CommentsController extends Controller
 {
