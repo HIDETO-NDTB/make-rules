ec2a1aac4c6d67f6293d018dbb5a6ed270dcdee9
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-10 16:06:46 +0530

    10.jan up

--- app/Comment.php
+++ app/Comment.php
@@ -8,5 +8,5 @@ class Comment extends Model
 {
     protected $table = 'rule_user';
 
-    protected $fillable = ['user_id','opinion','comment','comment_id','vote','voting_id'];
+    protected $fillable = ['rule_id','user_id','opinion','comment','comment_id','vote','voting_id'];
 }
