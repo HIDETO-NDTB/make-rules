8b2e3a5f2e3efd5e00ae92c47b70aa748f9c2840
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-10 14:32:03 +0530

    10.jan up

--- app/Http/Controllers/RuleUsersController.php
+++ app/Http/Controllers/RuleUsersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use RuleUser;
+use App\RuleUser;
 use Illuminate\Support\Facades\Session;
 
 class RuleUsersController extends Controller
