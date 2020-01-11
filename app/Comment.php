f85a5fb588eda1b1d5958e225b66444945e837e5
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-11 20:16:15 +0530

    11.jan up

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['rule_id','user_id','opinion','comment','comment_id'];
+    protected $fillable = ['rule_id','user_id','opinion','comment','comment_date'];
 
     protected $table = 'rule_user';
     protected $primaryKey = 'rule_id';
