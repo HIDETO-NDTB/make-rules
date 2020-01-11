4e78cff2b659223abe59abd7dd3cb4e1a0f3734b
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-11 19:54:20 +0530

    11.jan up

--- app/Comment.php
+++ app/Comment.php
@@ -9,4 +9,5 @@ class Comment extends Model
     protected $fillable = ['rule_id','user_id','opinion','comment','comment_id'];
 
     protected $table = 'rule_user';
+    protected $primaryKey = 'rule_id';
 }
