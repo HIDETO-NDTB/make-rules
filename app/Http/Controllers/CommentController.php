117c8cccf342112dca03db34dbddb7d0f28d0816
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-11 18:16:07 +0530

    11.jan up

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Comment;
+use App\Http\Requests\CreatePostRequest;
 
 class CommentController extends Controller
 {
@@ -33,7 +34,7 @@ class CommentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostRequest $request)
     {
         // validation
         $this->validate($request,[
