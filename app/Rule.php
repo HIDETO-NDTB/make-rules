852c9febba0fcac6cdca07260cd6151d6056f77e
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-12 14:06:49 +0530

    12.jan up

--- app/Rule.php
+++ app/Rule.php
@@ -17,4 +17,8 @@ class Rule extends Model
         return $this->belongsTo('App\User');
     }
 
+    public function users(){
+        return $this->belongsToMany('App\User');
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -39,6 +39,7 @@ class User extends Authenticatable
 
     public function rules() {
         return $this->hasMany('App\Rule');
+        return $this->belongsToMany('App\Rule');
     }
 
 }
