4c1e32b00f73a7025924d9bad5c9ac81d0e843de
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-12 13:11:29 +0530

    12.jan up

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -55,7 +55,7 @@ class CommentController extends Controller
         ]);
 
         $comment->save();
-        $comment->rules()->attach($comment->rules);
+        $rule->rules()->attach($comment->rules);
 
         //['id' =>$rule->id]
 
