b62b82bf00526ee700d9e18f5434fd75b07b6868
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-12 19:15:16 +0530

    12.jan up

---
+++ database/migrations/2020_01_12_134209_create_rule_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRuleUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rule_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('rule_id');
+            $table->integer('user_id');
+            $table->string('opinion');
+            $table->text('comment');
+            $table->date('comment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rule_user');
+    }
+}
