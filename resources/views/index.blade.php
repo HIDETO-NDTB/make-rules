87b153c9a6e7585ba7269df3510b4c99dbeb4c8a
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-13 16:17:42 +0530

    13.jan up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -131,7 +131,7 @@
                 </div>
             </div>
             <div class="commentButton">
-                <a class="btn btn-primary" style="width: 300px;" href="">詳しく見る</a>
+                <a class="btn btn-success" style="width: 300px;" href="">詳しく見る</a>
             </div>
         </div>
         <br>
