da57db287f34eb3962d732d5ccacd561a6806af0
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-13 19:46:20 +0530

    13.jan up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -136,8 +136,9 @@
                     <p class="comment-text">{{ $first_disagree_comment->comment }}</p>
                 </div>
                 <div class="comment-a">
-                    @if($first_agree_comment->rule_id == $first_disagree_comment->rule_id)
                     <h4 class="comment-title">賛成派意見</h4>
+                    @if($first_agree_comment->rule_id == $first_disagree_comment->rule_id)
+
                     <p class="comment-text">{{ $first_agree_comment->comment }}</p>
                     @endif
                 </div>
