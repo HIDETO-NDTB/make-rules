a7677dd7ff00fd697e731395227e9f268ed04c08
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-14 15:49:42 +0530

    14.jan up

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -61,7 +61,7 @@
                             <div class="col-lg-4">
                                 @if(var_dump(Carbon::today()->between(Carbon::parse($rule->initiative_date), Carbon::parse($rule->vote_date)));)
                                 <a class="btn btn-primary" style="width: 300px;" href="{{ route('rule.single',['id' =>$rule->id]) }}">コメントする</a>
-                                @elseif(var_dump(Carbon::today()->between(Carbon::parse($rule->vote_date), Carbon::parse($rule->vote_date(strtotime("+7 day"))));)
+                                @else
                                 <a class="btn btn-danger" style="width: 300px;" href="{{ route('rule.single',['id' =>$rule->id]) }}">投票に行く</a>
                                 @endif
                         </div>
