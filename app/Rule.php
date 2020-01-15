7247b395fbea674300b982c0ac15d6c593cb98cf
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-15 18:19:46 +0530

    15.jan up

--- app/Rule.php
+++ app/Rule.php
@@ -22,6 +22,8 @@ class Rule extends Model
         ->withPivot('opinion','comment','comment_date');
     }
 
-
+    public function vote(){
+        return $this->belongsTo('App\Vote');
+    }
 
 }
--- app/Vote.php
+++ app/Vote.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Vote extends Model
 {
     protected $fillable = ['rule_id','user_id','vote','vote_check','voting_date'];
+
+    public function rules(){
+        return $this->hasMany('App\Rule');
+    }
 }
+
