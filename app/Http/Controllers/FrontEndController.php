3d12185f148663a4345f070e3ca461f56ff2acc3
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-16 16:35:37 +0530

    16.jan up

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -67,10 +67,10 @@ class FrontEndController extends Controller
     }
 
 
-    public function category_single(Category $category){
+    public function category_single(Category $id){
 
         $rules = Category::find($category->id)->rules->sortBy('initiative_date');
-        $just_vote  = Rule::where('category_id',$category)->count();
+        $just_vote  = Rule::where('category_id',$id)->count();
 
         return view('category')->with('category',$category)
                                 ->with('rules',$rules)
