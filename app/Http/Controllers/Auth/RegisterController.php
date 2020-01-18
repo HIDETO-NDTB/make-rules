12ac1f83dd9c577cd10e905efa89a7f14a9532ac
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-18 15:36:31 +0530

    18.jan up

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'age' => $data['age'],
             'gender' => $data['gender'],
