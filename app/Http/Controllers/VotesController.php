5f10c1a4fafdeb98f56aeabd3b6811835b8160fc
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-20 21:00:46 +0530

    20.jan up

--- app/Http/Controllers/VotesController.php
+++ app/Http/Controllers/VotesController.php
@@ -47,9 +47,9 @@ class VotesController extends Controller
         ]);
 
         // 投票済かcheck
-        $usercheck = Vote::where('vote_check',"1")->where('user_id',Auth::user()->id)->get();
-        $rulecheck = Vote::where('vote_check',"1")->where('rule_id',$request->rule_id)->get();
-        if(isset($usercheck) && isset($rulecheck)){
+        $usercheck = Vote::where('vote_check',"1")->where('user_id',Auth::user()->id)->count();
+        $rulecheck = Vote::where('vote_check',"1")->where('rule_id',$request->rule_id)->count();
+        if(($usercheck == '1') && ($rulecheck == '1')){
 
             Session::flash('error','投票は法案に対し１回のみです');
             return redirect()->back();
