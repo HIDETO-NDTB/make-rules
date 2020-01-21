c98999a302b548e5eaf9fde14527d828081f8f55
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-21 14:47:01 +0530

    21.jan up

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Contact;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class ContactsController extends Controller
 {
