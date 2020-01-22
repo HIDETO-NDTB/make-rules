c4d2383889d9c5e61c7922ef4c0e83a661dd8dd6
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-22 11:30:53 +0530

    22.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,18 +1,16 @@
 <!-- Fixed navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                </button>
                 <a class="title" href="{{ route('index') }}">Make Rules</a>
             </div>
         </div>
         <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
             <div class="navbar-collapse collapse">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
                 @foreach ($categories as $category)
                 <ul class="nav navbar-nav">
                     <li><a class="btn btn-default" style="width:120px; margin: 10px 2px;" href="{{ route('category.single',['id' =>$category->id]) }}">{{ $category->name }}</a></li>
