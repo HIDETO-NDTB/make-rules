d6276b0feb77459334ec00b901e28e69fcd5d7a5
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-22 13:38:45 +0530

    22.jan up

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -7,10 +7,10 @@
                 <span class="icon-bar"></span>
             </button>
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-4 col-xs-9">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-9">
             <a class="title" href="{{ route('index') }}">Make Rules</a>
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-8 col-xs-3">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-0">
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="active"><a href="index.html">HOME</a></li>
@@ -20,6 +20,34 @@
                     <li><a data-toggle="modal" data-target="#myModal" href="#myModal"><i class="fa fa-envelope-o"></i></a></li>
                 </ul>
             </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="btn btn-primary" style="width: 100px; color:white;" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="btn btn-primary dropdown-toggle" style="font-size: 14px; color: white;" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>会員メニュー<span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('profile',['id' =>Auth::user()->id]) }}" style="font-size: 14px; color:black;">マイページ</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}" style="font-size: 14px; color:red;"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
         </div>
       <!--/.nav-collapse -->
     </div>
