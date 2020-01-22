78bac909a0701dd9b777343ee369383f9fd41713
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-22 12:59:31 +0530

    22.jan up

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -6,7 +6,9 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="title" href="{{ route('index') }}">Make Rules</a>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-9">
+                <a class="title" href="{{ route('index') }}">Make Rules</a>
+            </div>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
