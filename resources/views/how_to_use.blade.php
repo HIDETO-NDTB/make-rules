29f5e3bad0930893ba151ddb95c98286204eb6ab
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-23 14:45:43 +0530

    23.jan up

--- resources/views/how_to_use.blade.php
+++ resources/views/how_to_use.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row centered">
         <div class="col-lg-12">
-            <img src="{{ asset('app/img/How to use 2020-01-24.png') }}"  style="width:100%; height:100%;">
+            <img src="{{ asset('app/img/howuse 2020-01-24.png') }}"  style="width:100%; height:100%;">
         </div>
     </div>
 </div>
