fbd51abea4c29b0edf44fb1724d7ba306cdac45c
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-23 12:08:53 +0530

    23.jan up

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -84,7 +84,7 @@ class FrontEndController extends Controller
 
     public function category_single(Category $category){
 
-        $rules = Category::where('id',$category->id)->rules->sortBy('initiative_date');
+        $rules = Rule::where('category_id',$category->id)->orderBy('initiative_date');
         $just_vote  = Rule::where('category_id',$category->id)->whereDate('vote_date','<=',today())->whereDate('finish_date','>=',today())->count();
         $total_rule  = Rule::where('category_id',$category->id)->count();
         $comment_rule = Rule::where('category_id',$category->id)->whereDate('vote_date','>',today())->count();
