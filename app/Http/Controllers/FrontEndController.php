50bf65d9922ed2e1859eab73f5035f0bb3d34052
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-24 15:02:02 +0530

    24.jan up

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -172,9 +172,9 @@ class FrontEndController extends Controller
     public function result_total(){
 
         $rules = Rule::orderBy('created_at','desc')->whereDate('finish_date','<',today())->limit(30)->get();
-        $total_vote = Vote::where('rule_id',$rules->rule_id)->where('opinion',"")->count();
-        $total_agree = Vote::where('rule_id',$rules->rule_id)->where('vote','agree')->count();
-        $total_disagree = Vote::where('rule_id',$rules->rule_id)->where('vote','disagree')->count();
+        $total_vote = Vote::where('rule_id',$rules->rule->id)->where('opinion',"")->count();
+        $total_agree = Vote::where('rule_id',$rules->rule->id)->where('vote','agree')->count();
+        $total_disagree = Vote::where('rule_id',$rules->rule->id)->where('vote','disagree')->count();
         $comments = Comment::orderBy('created_at','desc')->get();
         $users = User::all();
 
