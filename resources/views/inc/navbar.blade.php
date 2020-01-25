5781dce976f6b6878c057d62db3cfb5c7d205e57
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 18:13:03 +0530

    25.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -9,7 +9,7 @@
             </button>
             <a class="title" href="{{ route('index') }}">Make Rules</a>
         </div>
-        <div class="col-lg-6 col-lg-offset-2 col-md-7 col-md-offset-0 col-sm-12">
+        <div class="col-lg-6 col-lg-offset-1 col-md-7 col-md-offset-0 col-sm-12">
             <div class="navbar-collapse collapse">
                 @foreach ($categories as $category)
                 <ul class="nav navbar-nav">
