3d260f0374a86838a7672595cbad260a453a0525
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 20:40:50 +0530

    25.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -9,7 +9,7 @@
             </button>
             <a class="title" href="{{ route('index') }}">Make Rules</a>
         </div>
-        <div class="col-lg-6 col-lg-offset-1 col-md-7 col-md-offset-0 col-sm-10 col-sm-offset-0 col-xs-6 col-xs-offset-0">
+        <div class="col-lg-6 col-lg-offset-1 col-md-7 col-md-offset-0 col-sm-9 col-sm-offset-0 col-xs-6 col-xs-offset-0">
             <div class="navbar-collapse collapse">
                 @foreach ($categories as $category)
                 <ul class="nav navbar-nav">
@@ -18,7 +18,7 @@
                 @endforeach
             </div>
         </div>
-        <div class="col-lg-3 col-lg-offset-0 col-md-4 col-md-offset-8 col-sm-8 col-sm-offset-4 col-xs-8 col-xs-offset-0">
+        <div class="col-lg-3 col-lg-offset-0 col-md-4 col-md-offset-8 col-sm-4 col-sm-offset-8 col-xs-6 col-xs-offset-0">
             <!-- Right Side Of Navbar -->
             <ul class="navbar-collapse collapse">
                 <!-- Authentication Links -->
