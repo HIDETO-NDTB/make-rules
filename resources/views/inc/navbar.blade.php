d21cb23c6a7fb8714f88259c2c72daee79fa81c4
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 17:27:11 +0530

    25.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,15 +1,15 @@
 <!-- Fixed navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
+        <div class="col-lg-3 col-md-4 col-sm-12 col-xs-6">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-12 col-xs-6">
             <a class="title" href="{{ route('index') }}">Make Rules</a>
+        </div>
         </div>
         <div class="col-lg-6 col-md-8 col-sm-12">
             <div class="navbar-collapse collapse">
