7c0d03a6034ad615f069b90b1f2850f7f8d6f1a4
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 14:20:01 +0530

    24.jan up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,6 +39,7 @@
             @isset($third_now_vote)
             <li><a href="{{ route('vote',['id' =>$third_now_vote->id]) }}">{{ $third_now_vote->title }}<br>({{ $third_now_vote->finish_date }} 投票終了)</a></li><br>
             @endisset
+            <a class="btn btn-success" style="width: 80%;" href="">投票中一覧</a>
         </div>
       <!-- col-lg-4 -->
         <div class="col-lg-4 col-lg-offset-0 col-md-4 col-md-offset-0 col-sm-4 col-sm-offset-0 col-xs-10 col-xs-offset-1">
