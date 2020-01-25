6c3c27844ec61d4a1f04ff29dd79b0c31c7d1d9a
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 19:38:55 +0530

    25.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -27,13 +27,10 @@
                         <a class="btn btn-primary" style="width: 100px; color:white;" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
                 @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="btn btn-primary dropdown-toggle" style="font-size: 14px; color: white;" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>会員メニュー<span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('profile',['id' =>Auth::user()->id]) }}" style="font-size: 14px; color:black;">マイページ</a><br>
-                            <a class="dropdown-item" href="{{ route('logout') }}" style="font-size: 14px; color:red;"
+                    <li class="nav-item">
+                        <div class="userButton">
+                            <a class="btn btn-prymary" href="{{ route('profile',['id' =>Auth::user()->id]) }}" style="font-size: 14px; color:black;">マイページ</a><br>
+                            <a class="btn btn-danger" href="{{ route('logout') }}" style="font-size: 14px; color:white;"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
