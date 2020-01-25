810a2d27931995bd4e5f76a381c599275743fbcf
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 17:29:34 +0530

    25.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- Fixed navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
-        <div class="col-lg-3 col-md-4 col-sm-12 col-xs-6">
+
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="icon-bar"></span>
@@ -10,7 +10,7 @@
             </button>
             <a class="title" href="{{ route('index') }}">Make Rules</a>
         </div>
-        </div>
+
         <div class="col-lg-6 col-md-8 col-sm-12">
             <div class="navbar-collapse collapse">
                 @foreach ($categories as $category)
