04ffad212b02dd865d28f0ebad67c1d1fbbfd037
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 14:10:29 +0530

    24.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -34,7 +34,7 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('profile',['id' =>Auth::user()->id]) }}" style="font-size: 14px; color:black;">マイページ</a>
+                            <a class="dropdown-item" href="{{ route('profile',['id' =>Auth::user()->id]) }}" style="font-size: 14px; color:black;">マイページ</a><br>
                             <a class="dropdown-item" href="{{ route('logout') }}" style="font-size: 14px; color:red;"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
