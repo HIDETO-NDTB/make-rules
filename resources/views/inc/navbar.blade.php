670d7c2a972dd601cd4a65b3317c0b44f585bf8b
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 18:43:03 +0530

    25.jan up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -18,7 +18,7 @@
                 @endforeach
             </div>
         </div>
-        <div class="col-lg-3 col-lg-offset-0 col-md-2 col-md-offset-0 col-sm-2 col-sm-offset-10 col-xs-3">
+        <div class="col-lg-3 col-lg-offset-0 col-md-2 col-md-offset-0 col-sm-2 col-sm-offset-8 col-xs-3">
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
