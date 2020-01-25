30ec17591d1ceb2bc4c8af23571116c053dda221
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 13:13:14 +0530

    25.jan up

--- resources/views/result_total.blade.php
+++ resources/views/result_total.blade.php
@@ -19,7 +19,7 @@
             @foreach ($rules as $rule)
                 <div class="card">
                     <div class="card-header"><h4>{{ $rule->title }}</h4></div>
-                    <div class="card-header"><h4>カテゴリ: {{ $rule->category->name }}</h4></div>
+                    <div class="category"><h4>{{ $rule->category->name }}</h4></div>
                     <div class="card-body">
                         <ul>
                             <div class="row">
