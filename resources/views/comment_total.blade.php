7558c27675fcb40c5eaf80fa1bd066b8edb04a9c
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-01-25 16:02:47 +0530

    25.jan up

--- resources/views/comment_total.blade.php
+++ resources/views/comment_total.blade.php
@@ -57,7 +57,7 @@
                             <div class="row">
                                 <div class="col-lg-8"></div>
                                 <div class="col-lg-4">
-                                <a class="btn btn-primary" style="width: 300px;" href="{{ route('rule_single',['id' =>$rule->id]) }}">コメントする</a>
+                                <a class="btn btn-primary" style="width: 300px;" href="{{ route('rule.single',['id' =>$rule->id]) }}">コメントする</a>
                             </div>
                         </ul>
                     </div>
