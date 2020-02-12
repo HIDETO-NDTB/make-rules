bc79c3d00d3938a129436fa22672ebc11d73684e
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-12 23:43:24 +0900

    12 Feb up

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -77,7 +77,7 @@ class RegisterController extends Controller
 
         $profile = Profile::create([
             'user_id' => $user->id,
-            'avatar' => asset('sample.jpg'),
+            'avatar' => asset('uploads/avatar/sample.jpg'),
 
         ]);
 
