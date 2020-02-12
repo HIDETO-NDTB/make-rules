0d3c2c8936bcdfb9e4c22742924359fd2d0faff0
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-12 20:37:00 +0900

    12 Feb up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        $url->forceScheme('https');
+        $url->forceSchema('https');
     }
 }
