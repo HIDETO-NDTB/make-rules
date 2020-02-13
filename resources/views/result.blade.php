7e3cfc1228f0f0dcfa8d4b2f9fd7b5d8eb35fcac
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-13 21:20:35 +0900

    13 Feb up

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -13,7 +13,7 @@
                                 <div class="col-lg-2 col-md-3 col-sm-4 col-xs-4">
                                     @foreach ($users as $user)
                                     @if($rule->user_id == $user->id)
-                                    <li><img src="{{ asset(url($user->profile->avatar)) }}" class="img-responsive img-circle" style="width:60px; height:60px;" alt="avator"></li>
+                                    <li><img src="{{ secure_asset(url($user->profile->avatar)) }}" class="img-responsive img-circle" style="width:60px; height:60px;" alt="avator"></li>
                                     @endif
                                     @endforeach
                                 </div>
