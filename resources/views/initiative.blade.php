1320ccc42e5e2b1d12d5c5ad1c42efe519224af9
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-14 14:26:33 +0900

    14 Feb up

--- resources/views/initiative.blade.php
+++ resources/views/initiative.blade.php
@@ -16,7 +16,7 @@
     <div class="container">
         <div class="row centerd">
             <div class="col-lg-12">
-                <h4><span id="initiativetitle"> 新法案の発議 </span></h4>
+                <h4><span> 新法案の発議 </span></h4>
                 <form action="{{ route('rules.store') }}" method="POST">
                     @csrf
                     <div class="form-group">
