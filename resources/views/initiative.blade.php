d8f1152781fc2d9eb2ac9e2c7045c2d3d5862fc2
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-14 15:48:10 +0900

    14 Feb up

--- resources/views/initiative.blade.php
+++ resources/views/initiative.blade.php
@@ -22,7 +22,7 @@
                     <div class="form-group">
                         <label>タイトル（20文字以内）</label>
                         <input type="text" name="title" class="form-control" id="nput_sample" placeholder="タイトルを入力下さい" value="">
-                        <div class="count">入力文字数: <span id="input_result">0</span></div>
+                        <p id="input_result"></p>
                     </div>
                     <script>
                     $(function(){
