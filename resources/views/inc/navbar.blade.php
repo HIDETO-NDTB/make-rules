3c7d2d15e37356b6e7c2204fb8302fd1ee688ce7
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-15 20:18:04 +0900

    15 Feb up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,36 +1,33 @@
 <!-- Fixed navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="title" href="{{ route('index') }}">Make Rules</a>
+    <div class="container">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="title" href="{{ route('index') }}">Make Rules</a>
+    
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+            @foreach ($categories as $category)
+                <li><a class="btn" href="{{ route('category.single',['id' =>$category->id]) }}">{{ $category->name }}</a></li>
+            @endforeach
+            </ul>
         </div>
-        <div class="col-lg-6 col-lg-offset-1 col-md-7 col-md-offset-0 col-sm-10 col-sm-offset-0 col-xs-6 col-xs-offset-0">
-            <div class="navbar-collapse collapse">
-                @foreach ($categories as $category)
-                <ul class="nav navbar-nav">
-                    <li><a class="btn btn-default" style="width:105px; margin: 10px 2px;" href="{{ route('category.single',['id' =>$category->id]) }}">{{ $category->name }}</a></li>
-                </ul>
-                @endforeach
-            </div>
-        </div>
-        <div class="col-lg-3 col-lg-offset-0 col-md-4 col-md-offset-8 col-sm-5 col-sm-offset-7 col-xs-6 col-xs-offset-0">
-            <!-- Right Side Of Navbar -->
+
+        <div class="navbar-login">
             <ul class="navbar-collapse collapse">
                 <!-- Authentication Links -->
                 @guest
                     <li class="nav-item">
-                        <a class="btn btn-primary" style="font-size: 14px; width: 100px; color:white;" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+                        <a class="btn" href="{{ route('login') }}">{{ __('ログイン') }}</a>
                     </li>
                 @else
                     <li class="nav-item">
                         <div class="userButton">
-                            <a class="btn btn-primary" href="{{ route('profile',['id' =>Auth::user()->id]) }}" style="font-size: 14px; color:white; width:100px;">マイページ</a>
-                            <a class="btn btn-danger" href="{{ route('logout') }}" style="font-size: 14px; color:white; width:100px;"
+                            <a class="btn" href="{{ route('profile',['id' =>Auth::user()->id]) }}">マイページ</a>
+                            <a class="btn" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 {{ __('ログアウト') }}
