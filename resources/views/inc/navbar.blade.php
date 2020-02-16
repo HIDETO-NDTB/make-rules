b09bd02dd8a5e405e68b953fd0f2ab1a52bcea3e
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-16 15:58:09 +0900

    15 Feb up

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -12,11 +12,11 @@
             <ul class="nav navbar-nav">
                 <li class="nav-item dropdown">
                     <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">カテゴリ一覧</button>
-                    <div class="dropdown-menu">
                     @foreach ($categories as $category)
+                    <div class="dropdown-menu">
                         <a class="dropdown-item" href="{{ route('category.single',['id' =>$category->id]) }}">{{ $category->name }}</a>
-                    @endforeach
                     </div>
+                    @endforeach
                 </li>
 
                     @guest
