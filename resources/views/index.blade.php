d80be9477405fec90fcc616287ed0843e40fc6b3
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-17 16:33:26 +0900

    17 Feb up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -213,7 +213,7 @@
                 </div>
             </div>
             <div class="commentButton">
-                <a class="btnwhite" href="{{ route('rule.single',['id' =>$first_disagree_comment->rule_id]) }}">詳しく見る</a>
+                <a class="btn" href="{{ route('rule.single',['id' =>$first_disagree_comment->rule_id]) }}">詳しく見る</a>
             </div>
         </div>
         @endif
