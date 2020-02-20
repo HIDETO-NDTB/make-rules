2c62ec760cd0e53eae8b8f0f6da6a0aae7583f09
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-20 23:02:07 +0900

    20 Feb up

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -10,12 +10,12 @@
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('メールが送信されました。') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="">{{ __('click here to request another') }}</a>.
+                    {{ __('メールを送信しました。メールからメールアドレスの認証をお願いします。<br>') }}
+                    {{ __('メールが届いていない場合は、') }}, <a href="">{{ __('ここをクリックして下さい') }}</a>。再送致します。
                 </div>
             </div>
         </div>
