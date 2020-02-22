bc61089202e230b76df1841ae2483d5d0d7826f3
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-22 20:11:26 +0900

    21 Feb up

--- app/Http/Controllers/SiteMapController.php
+++ app/Http/Controllers/SiteMapController.php
@@ -5,22 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
+use App\Rule;
 
 class SiteMapController extends Controller
 {
-    // 画面表示
-    public function html()
-    {
-        return view('sitemap');
-    }
-
-    // sitemap.xmlの生成と表示
-    public function xml()
+    public function sitemap()
     {
         $sitemap = \App::make("sitemap");
         $now = Carbon::now();
-        $sitemap->add(url('/'), $now, '1.0', 'daily');
-        $sitemap->store('xml', 'sitemap');
+        $sitemap->add(url('/'), $now, '1.0', 'alweys');
+        
+
+        $rules = Rule::orderBy('created_at','desc')->get();
+        foreach ($rules as $rule)
+        {
+            $sitemap->add(URL::to('/rules/' . $rule->id), $rukle->created_at, '0.8', 'yearly');
+        }
+        
 
         return $sitemap->render('xml');
     }
