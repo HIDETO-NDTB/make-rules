92207c179b4da65d7c672280adfdf8eb50f5be0b
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-22 21:47:38 +0900

    21 Feb up

--- resources/views/introduce.blade.php
+++ resources/views/introduce.blade.php
@@ -1,6 +1,40 @@
-<?php 
+@extends('layouts.front')
+@section('page')
 
-echo 'hello world';
+<div id="headerwrap">
+    <div class="container">
+        <div class="row centered">
+            <div class="col-lg-6">
+                <div class="initiative-option">
+                    <h5 class="introduce">Make Rulesを学校や職場などで使いませんか？</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="contentsintroduce">
+    <div class="container">
+        <h2>Make Rules 単独利用契約のメリット</h2>
+        <div class="contentdetale">
+            <h4>1. 学校や職場の仲間だけで使える</h4>
+            <h4>2. 思考力、説得力、理解力の育成に繋がる</h4>
+            <h4>3. 実際のニュースに関心を持つようになる</h4>
+            <h4>4. 日々の生活に問題意識が芽生える</h4>
+            <h4>5. 月29,700円〜。最短３ヶ月契約OK</h4>
+            <p>※ プランによって異なります</p>
+            <h4>6. カテゴリや期間などカスタマイズも相談可</h4>
+            <p>※ 内容によっては別料金となります</p>
+            <h4>7. 規則の大切さに気づく</h4>
+        </div>
+    </div>
+    <div class="container">
+        <h2>Make Rules 商品のご紹介</h2>
+        <table class="contentfee" border="1" cellpadding="5" cellspacing="5" width="100%">
+            <h4>プラン1. 30人未満プラン</h4>
+            
+        </div>
+    </div>
+</div>
 
 
-?>
+@endsection
