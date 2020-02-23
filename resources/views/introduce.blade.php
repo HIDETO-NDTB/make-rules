149b5ed0cd2e055bffa59e28b961bc70ec542b70
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-23 19:12:36 +0900

    23 Feb up

--- resources/views/introduce.blade.php
+++ resources/views/introduce.blade.php
@@ -14,21 +14,33 @@
 </div>
 <div id="contentsintroduce">
     <div class="container">
-        <h3>Make Rules 単独利用契約のメリット</h3>
+        <h3>単独利用契約のメリット</h3>
         <div class="contentdetale">
             <h4>1. 学校や職場の仲間だけで使える</h4>
+        </div>
+        <div class="contentdetale">
             <h4>2. 思考力、説得力、理解力の育成に繋がる</h4>
+        </div>
+        <div class="contentdetale">
             <h4>3. 実際のニュースに関心を持つようになる</h4>
+        </div>
+        <div class="contentdetale">
             <h4>4. 日々の生活に問題意識が芽生える</h4>
+        </div>
+        <div class="contentdetale">
             <h4>5. 月29,700円〜。最短３ヶ月契約OK</h4>
             <p>※ プランによって異なります</p>
+        </div>
+        <div class="contentdetale">
             <h4>6. カテゴリや期間などカスタマイズも相談可</h4>
             <p>※ 内容によっては別料金となります</p>
+        </div>
+        <div class="contentdetale">
             <h4>7. 規則の大切さに気づく</h4>
         </div>
     </div>
     <div class="container">
-        <h2>Make Rules 商品のご紹介</h2>
+        <h2>商品のご紹介</h2>
         <table class="contentfee" border="1" cellpadding="5" cellspacing="5" width="100%">
             <h4>プラン1. 30人未満プラン</h4>
             
