fab5a362fd91c0752e2125b5042eaf340c79d2eb
EC2 Default User <ec2-user@ip-172-31-28-199.us-east-2.compute.internal>
2020-02-23 19:38:58 +0900

    23 Feb up

--- resources/views/introduce.blade.php
+++ resources/views/introduce.blade.php
@@ -12,33 +12,39 @@
         </div>
     </div>
 </div>
-<div id="contentsintroduce">
-    <div class="container">
-        <h3>単独利用契約のメリット</h3>
-        <div class="contentdetale">
-            <h4>1. 学校や職場の仲間だけで使える</h4>
+<div class="col-lg-6">
+    <img src="{{ secure_asset('app/img/school-min.jpg') }}">
+    <img src="{{ secure_asset('app/img/office-min.jpg') }}">
+</div>
+<div class="col-lg-6">
+    <div id="contentsintroduce">
+        <div class="container">
+            <h3>単独利用契約のメリット</h3>
+            <div class="contentdetale">
+                <h4>1. 学校や職場の仲間だけで使える</h4>
+            </div>
+            <div class="contentdetale">
+                <h4>2. 思考力、説得力、理解力の育成に繋がる</h4>
+            </div>
+            <div class="contentdetale">
+                <h4>3. 実際のニュースに関心を持つようになる</h4>
+            </div>
+            <div class="contentdetale">
+                <h4>4. 日々の生活に問題意識が芽生える</h4>
+            </div>
+            <div class="contentdetale2">
+                <h4>5. 月●●●●●円〜。最短３ヶ月契約OK</h4>
+                <p>※ プランによって異なります</p>
+            </div>
+            <div class="contentdetale2">
+                <h4>6. カテゴリや投票期間などカスタマイズも相談可</h4>
+                <p>※ 内容によっては別料金となります</p>
+            </div>
+            <div class="contentdetale">
+                <h4>7. 規則の大切さに気づく</h4>
+            </div>
         </div>
-        <div class="contentdetale">
-            <h4>2. 思考力、説得力、理解力の育成に繋がる</h4>
-        </div>
-        <div class="contentdetale">
-            <h4>3. 実際のニュースに関心を持つようになる</h4>
-        </div>
-        <div class="contentdetale">
-            <h4>4. 日々の生活に問題意識が芽生える</h4>
-        </div>
-        <div class="contentdetale2">
-            <h4>5. 月●●●●●円〜。最短３ヶ月契約OK</h4>
-            <p>※ プランによって異なります</p>
-        </div>
-        <div class="contentdetale2">
-            <h4>6. カテゴリや投票期間などカスタマイズも相談可</h4>
-            <p>※ 内容によっては別料金となります</p>
-        </div>
-        <div class="contentdetale">
-            <h4>7. 規則の大切さに気づく</h4>
-        </div>
-    </div>
+</div>
     <div class="container">
         <h2>商品のご紹介</h2>
         <table class="contentfee" border="1" cellpadding="5" cellspacing="5" width="100%">
