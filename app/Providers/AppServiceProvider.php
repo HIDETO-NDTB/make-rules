68da94c4782b6c15ab3e4738dbf6bb88ca3234d6
HIDETO-NDTB <nackwisteria@yahoo.co.jp>
2020-03-01 16:42:24 +0900

    ssl化解消

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         // $url->forceScheme('https');
     }
