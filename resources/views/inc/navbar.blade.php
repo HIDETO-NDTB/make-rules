9c178abc4ab12507b4fc45ede754b6bc1424695d
HIDETO-NDTB <nackwisteria@yahoo.co.jp>
2020-03-01 21:13:52 +0900

    管理者レジスター

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -42,9 +42,9 @@
                     <a class="navbtn" href="{{ route('how_to_use') }}">利用方法</a>
                 </li>
                 @if(Auth::check() && Auth::user()->admin == 1)
-                <li class="nav-admin">
+                
                 <a class="btn btn-success" href="{{ route('manage') }}" style="width: 140px">管理者メニュー</a>
-                </li>
+                
                 @endif
             </ul>
         </div>
