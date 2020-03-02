0598395ede193b96a9b1766ffc7ea861db1af4f0
HIDETO-NDTB <nackwisteria@yahoo.co.jp>
2020-03-02 21:55:58 +0900

    管理者show確認ページ

--- resources/views/ruleshow.blade.php
+++ resources/views/ruleshow.blade.php
@@ -15,8 +15,8 @@
     @foreach ($rules as $rule)
       <tr class="user-table">
         <td>{{ $rule->id }}</td>
-        <td>{{ $rule->title }}</td>
-        <td>{{ $rule->content }}</td>
+        <td  class="left">{{ $rule->title }}</td>
+        <td  class="left">{{ $rule->content }}</td>
         
         <td>{{ $rule->initiative_date }}</td>
         <td><a class="btn btn-danger" href="" style="width: 100px;">削除</a></td>
