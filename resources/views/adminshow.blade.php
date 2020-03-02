8dc4dc8795a4d2b58d4a58bd2e8dab4f24af8b3a
HIDETO-NDTB <nackwisteria@yahoo.co.jp>
2020-03-02 22:40:55 +0900

    管理者userページ

--- resources/views/adminshow.blade.php
+++ resources/views/adminshow.blade.php
@@ -1,27 +1,29 @@
 @extends('layouts.front')
 @section('page')
 
-<div class="user-table">
-<table class="table">
-    <tr class="user-table">
-        <th>ID</th>
-        <th>名前</th>
-        <th>年齢</th>
-        <th>性別</th>
-        <th>Eメール</th>
-        <th>管理者操作</th>
-    </tr>
-    @foreach ($users as $user)
-      <tr class="user-table">
-        <td>{{ $user->id }}</td>
-        <td>{{ $user->name }}</td>
-        <td>{{ $user->age }}</td>
-        <td>{{ $user->gender }}</td>
-        <td>{{ $user->email }}</td>
-        <td><a class="btn btn-danger" href="{{ route('users.show',['id' =>$user->id]) }}" style="width: 100px;">削除</a></td>
-      </tr>
-    @endforeach
-</table>
+<div class="container">
+    <div class="user-table">
+        <table class="table">
+            <tr class="user-table">
+                <th>ID</th>
+                <th>名前</th>
+                <th>年齢</th>
+                <th>性別</th>
+                <th>Eメール</th>
+                <th>管理者操作</th>
+            </tr>
+            @foreach ($users as $user)
+              <tr class="user-table">
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->age }}</td>
+                <td>{{ $user->gender }}</td>
+                <td>{{ $user->email }}</td>
+                <td><a class="btn btn-danger" href="{{ route('users.show',['id' =>$user->id]) }}" style="width: 100px;">削除</a></td>
+              </tr>
+            @endforeach
+        </table>
+    </div>
 </div>
 
 @endsection
