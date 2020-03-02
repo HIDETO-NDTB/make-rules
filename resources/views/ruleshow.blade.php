8bc1be47d413b781cb40a05351d6e87951e2474d
HIDETO-NDTB <nackwisteria@yahoo.co.jp>
2020-03-02 22:17:29 +0900

    管理者ruleページ

--- resources/views/ruleshow.blade.php
+++ resources/views/ruleshow.blade.php
@@ -7,7 +7,7 @@
         <th>ID</th>
         <th>Title</th>
         <th>内容</th>
-        <th>カテゴリ</th>
+        
         <th>ユーザー名</th>
         <th>発議日</th>
         <th>管理者操作</th>
@@ -17,7 +17,7 @@
         <td>{{ $rule->id }}</td>
         <td  class="left">{{ $rule->title }}</td>
         <td  class="left">{{ $rule->content }}</td>
-        <td>{{ $rule->category->name }}</td>
+        
         <td>{{ $rule->user->name }}</td>
         <td>{{ $rule->initiative_date }}</td>
         <td><a class="btn btn-danger" href="" style="width: 100px;">削除</a></td>
